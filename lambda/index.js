a93db2a981f5be2cfcc906983d01d254faff4bd4
Nathan LaFreniere <quitlahok@gmail.com>
2017-07-06 14:26:04 -0700

    change to be triggered by s3

--- lambda/index.js
+++ lambda/index.js
@@ -6,34 +6,29 @@ const S3 = new AWS.S3({
 });
 const Sharp = require('sharp');
 
-const BUCKET = process.env.BUCKET;
-const URL = process.env.URL;
+const WIDTH = 450;
+const HEIGHT = 450;
 
 exports.handler = function(event, context, callback) {
-  const key = event.queryStringParameters.key;
-  const match = key.match(/(\d+)x(\d+)\/(.*)/);
-  const width = parseInt(match[1], 10);
-  const height = parseInt(match[2], 10);
-  const originalKey = match[3];
+  const key = event.Records[0].s3.object.key;
+  const bucket = event.Records[0].s3.bucket.name;
 
-  S3.getObject({Bucket: BUCKET, Key: originalKey}).promise()
-    .then(data => Sharp(data.Body)
-      .resize(width, height)
-      .toFormat('png')
-      .toBuffer()
-    )
-    .then(buffer => S3.putObject({
-        Body: buffer,
-        Bucket: BUCKET,
-        ContentType: 'image/png',
-        Key: key,
-      }).promise()
-    )
-    .then(() => callback(null, {
-        statusCode: '301',
-        headers: {'location': `${URL}/${key}`},
-        body: '',
-      })
-    )
+  S3.getObject({Bucket: bucket, Key: key}).promise()
+    .then((data) => {
+
+      Sharp(data.Body)
+        .resize(WIDTH, HEIGHT)
+        .toBuffer()
+        .then((buffer) => {
+
+          return S3.putObject({
+            Body: buffer,
+            Bucket: bucket,
+            ContentType: data.ContentType,
+            Key: `450x450/${key}`
+          }).promise();
+        });
+    })
+    .then(() => callback(null, `successfully resized ${key}`))
     .catch(err => callback(err))
 }
